c44665c36ea45e12de42c819ce157459b9b474cf
Alejandro Melara <alejandro.melara96@gmail.com>
2023-03-14 19:46:53 -0400

    Separate code into components

---
+++ src/components/Display.jsx
@@ -0,0 +1,9 @@
+export default function Display() {
+  return (
+    <>
+      <h1 className="text-4xl font-black">Name or Initials</h1>
+      <input type="text" className=" rounded bg-black text-white" />
+      <button className=" rounded bg-black px-6 text-white">Go</button>
+    </>
+  );
+}
---
+++ src/components/WordGuess.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+const WORD = "bird";
+const wordSeparator = WORD.split("");
+console.log(wordSeparator);
+
+export default function WordGuess() {
+  const [wordChange, setWordChange] = useState("_");
+  const [wordChangeSecondLetter, setWordChangeSecondLetter] = useState("_");
+  const [wordChangeThirdLetter, setWordChangeThirdLetter] = useState("_");
+  const [wordChangeFourthLetter, setWordChangeFourthLetter] = useState("_");
+
+  return (
+    <>
+      <h1 className="text-4xl font-black">Guess the Word</h1>
+
+      <p className="text-8xl font-extrabold uppercase tracking-widest">
+        {wordChange}
+        {wordChangeSecondLetter}
+        {wordChangeThirdLetter}
+        {wordChangeFourthLetter}
+      </p>
+
+      <div className="flex gap-x-2">
+        <label htmlFor="guess">Guess a letter:</label>
+        <input
+          type="text"
+          className="w-8 border-2 border-gray-300"
+          maxLength={0}
+          id="guess"
+          onKeyDown={() => {
+            if (event.key === wordSeparator[0]) {
+              setWordChange(wordSeparator[0]);
+            } else if (event.key === wordSeparator[1]) {
+              setWordChangeSecondLetter(wordSeparator[1]);
+            } else if (event.key === wordSeparator[2]) {
+              setWordChangeThirdLetter(wordSeparator[2]);
+            } else if (event.key === wordSeparator[3]) {
+              setWordChangeFourthLetter(wordSeparator[3]);
+            }
+          }}
+        />
+      </div>
+    </>
+  );
+}
