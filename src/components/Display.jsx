95ab80618ca0b588dabb79ce95192860be2045f3
Alejandro Melara <alejandro.melara96@gmail.com>
2023-03-14 20:16:04 -0400

    Lift state up to handleclick

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,9 +1,20 @@
-export default function Display() {
+import PropTypes from "prop-types";
+
+export default function Display({ setShow }) {
   return (
     <>
       <h1 className="text-4xl font-black">Name or Initials</h1>
-      <input type="text" className=" rounded bg-black text-white" />
-      <button className=" rounded bg-black px-6 text-white">Go</button>
+      <input type="text" className=" rounded bg-black text-center text-white" />
+      <button
+        onClick={() => setShow(false)}
+        className=" rounded bg-black px-6 text-white"
+      >
+        Go
+      </button>
     </>
   );
 }
+
+Display.propTypes = {
+  setShow: PropTypes.func.isRequired,
+};
